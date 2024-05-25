a34f04bad6a0b3ecb384fa3cf03e46be6980d8a5
Pigeon <146737777+pid-j@users.noreply.github.com>
2024-05-25 14:30:16 +0300

    Create installer.sh [TESTING]

---
+++ installer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [[ ! -d ".repos" ]] ; then
+  git clone --filter=blob:none --no-checkout "github.com/pid-j/PigeonPkgDatabase.git" ".repos"
+fi
+
+cd ".repos"
+if [[ ! -d "$1" ]] ; then
+  git sparse-checkout set "$1"
+
+  cd "$1"
+  if [[ -f requirements.txt ]] ; then
+    cat requirements.txt || while read line; do
+      if [[ ! -d "../$1" ]] ; then $0 line; fi
+    done
+  fi
+fi
+
+echo Success
