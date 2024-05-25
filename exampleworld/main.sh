b7225023e5cab175d37f05fe2c1950a9f87b871f
Pigeon <146737777+pid-j@users.noreply.github.com>
2024-05-25 15:09:29 +0300

    Update main.sh

--- exampleworld/main.sh
+++ exampleworld/main.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # This is just an example file.
 for EXAMPLEINDEX in $(seq 1 16); do
-  echo "$(../helloworld/main.sh) $EXAMPLEINDEX"
+  echo "$(bash ../helloworld/main.sh) $EXAMPLEINDEX"
 done
